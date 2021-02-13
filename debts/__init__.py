b170fd491b7595102c44d68e0a16616b36978efc
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2021-02-13 14:48:34 -0500

    added basic functionality and test placeholder

---
+++ debts/__init__.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import sys
+
+'''
+def sum(arg):
+	total = 0 
+	for val in arg:
+		total += val
+	return total
+'''
+
+# outputs a list of dictionaries to stdout in JSON LINES format 
+def write_jsonl(items):
+	for item in items:
+		item_json = json.dumps(item)
+		sys.stdout.write(item_json)
+		sys.stdout.write("\n")
+		
+def get_resource(url):
+	try:
+		response = requests.get(url, verify=False, timeout=5).json()
+		write_jsonl(response)
+		return response
+	except requests.exceptions.RequestException as e: #Check this
+		#raise Exception("Request from ", url, " failed.");
+		print("Something went wrong")
+		raise SystemExit(e)
+
+def process_debts_payments():
+	
+	base_url = 'https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/'
+	
+	debts_url = base_url + 'debts'
+	payment_plans_url = base_url + 'payment_plans'
+	payments_url = base_url + 'payments'
+
+	print("\n\nDEBTS")
+	debts = get_resource(debts_url)
+	print("\n\nPAYMENT PLANS")
+	payment_plans = get_resource(payment_plans_url)
+	print("\n\nPAYMENTS")
+	payments = get_resource(payments_url)
+
+process_debts_payments();
+
+
+ 
+
---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from debts import process_debts_payments
+
+class TestSum(unittest.TestCase):
+	
+	'''
+	#def test_process_debts_payments(self):
+	#	testDebts = get_debts();
+	#	self.assertEqual(testDebts[0].status_code, 200);
+	
+	#def test_is_JSON_line(self):
+	#	data = [1,2,3];
+	#	result = sum(data);
+	#	self.assertEqual(result, 6, "Should be 6");
+	'''
+		
+if __name__ == '__main__':
+	unittest.main()
